ae8be3f730141fd3d80b1c5e5857589a72e91f38
Andre Luis de Aguiar Lopes <andrelopes.al@gmail.com>
2024-04-26 11:59:53 -0300

    Adicionada screen form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_flutter/screens/form_screen.dart';
 import 'package:primeiro_flutter/screens/initial_screen.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
       ),
       home: const InitialScreen(),
+      //home: const FormScreen(),
     );
   }
 }
